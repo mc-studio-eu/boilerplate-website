f6de73c7bbfc6b99c806331056db0442f21ae73b
mohamedDev69 <mohamedchettah0208@gmail.com>
2026-01-25 14:52:15 +0100

    feat: config nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,46 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/i18n'],
   css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: "MC Studio | Création d'expériences digital",
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Le Studio de Design & Développement' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { property: 'og:title', content: "MC Studio" },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://www.mc-studio.eu' },
+        { property: 'og:image', content: 'https://www.mc-studio.eu/img/fond-site.jpg' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon-new.png' },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon-new.png' }
+      ],
+      script: [
+        {
+          innerHTML: `(function(){document.documentElement.classList.add('dark');document.documentElement.style.colorScheme='dark';localStorage.setItem('nuxt-color-mode','dark');})();`,
+          type: 'text/javascript'
+        }
+      ]
+    }
+  },
+
   colorMode: {
     preference: 'dark',
     fallback: 'dark',
     classSuffix: '',
+    storage: 'localStorage',
+    storageKey: 'nuxt-color-mode',
   },
   i18n: {
     locales: [
